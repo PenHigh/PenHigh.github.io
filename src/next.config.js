bc4e24e3c166f139dee6d88d398d2107bb0ec72f
Eroxl <InfinityWaffles@outlook.com>
2023-01-12 19:03:28 -0800

    Remove default files from create-next-app

--- src/next.config.js
+++ src/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-const BASE_PATH = process.env.NEXTJS_BASE_PATH
-
 const nextConfig = {
   reactStrictMode: true,
-  basePath: BASE_PATH ? BASE_PATH : ""
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
