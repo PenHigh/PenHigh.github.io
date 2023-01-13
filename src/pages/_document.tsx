d61acb3c3db5ec454f6aee0e9bc9f59d8c3e5489
Eroxl <InfinityWaffles@outlook.com>
2023-01-12 19:24:22 -0800

    Change site wide font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import {
   Html, Head, Main, NextScript,
 } from 'next/document';
+import { Signika } from '@next/font/google';
+
+const signikaFont = Signika();
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className={signikaFont.className}>
         <Main />
         <NextScript />
       </body>
