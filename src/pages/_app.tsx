5544267d08b91ac8d6484cabbb7b5666350a29f6
Eroxl <InfinityWaffles@outlook.com>
2023-01-12 19:27:35 -0800

    Use correct font subset

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,19 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import type { AppProps } from 'next/app';
 import { Signika } from '@next/font/google';
 
 import '../styles/globals.css';
 
-const signikaFont = Signika();
-
+const signikaFont = Signika(
+  {
+    subsets: ['latin'],
+  },
+);
 
 const App = ({ Component, pageProps }: AppProps) => (
   <div className={signikaFont.className}>
-    <Component {...pageProps} />;
+    <Component {...pageProps} />
   </div>
 );
 
