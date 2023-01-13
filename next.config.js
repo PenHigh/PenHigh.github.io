d2a934b8343e79b275b8fbeb0f8ff899b2e6a9a3
Toby <newtobycm@gmail.com>
2023-01-12 16:57:17 -0800

    i mean what

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const BASE_PATH = process.env.NEXTJS_BASE_PATH
+
 const nextConfig = {
   reactStrictMode: true,
+  basePath: BASE_PATH ? BASE_PATH : ""
 }
 
 module.exports = nextConfig
