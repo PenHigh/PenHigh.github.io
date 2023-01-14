a163d228646abc6f2e5ccd763a1841d0e13fb742
Eroxl <InfinityWaffles@outlook.com>
2023-01-13 21:19:44 -0800

    Revert image changes

--- src/next.config.js
+++ src/next.config.js
@@ -3,15 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: '',
   images: {
-    loader: 'custom',
     path: '/',
-  },
-  env: {
-    nextImageExportOptimizer_imageFolderPath: 'public/images',
-    nextImageExportOptimizer_exportFolderPath: 'out',
-    nextImageExportOptimizer_quality: 75,
-    nextImageExportOptimizer_storePicturesInWEBP: true,
-    nextImageExportOptimizer_generateAndUseBlurImages: true,
   },
 };
 
