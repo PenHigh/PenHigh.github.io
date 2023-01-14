3b6a3799388cf086bdc02425aaf726bb0966d9bd
tobycm <62174797+tobycm@users.noreply.github.com>
2023-01-13 20:49:23 -0800

    i remove bloat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,10 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import type { AppProps } from 'next/app';
-import { Signika } from '@next/font/google';
-
 import '../styles/globals.css';
 
-const signikaFont = Signika(
-  {
-    subsets: ['latin'],
-  },
-);
-
 const App = ({ Component, pageProps }: AppProps) => (
-  <div className={signikaFont.className}>
+  <div>
     <Component {...pageProps} />
   </div>
 );
