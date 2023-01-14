bcbd69384f2e99143bd1af9cf470b173c9401659
Eroxl <InfinityWaffles@outlook.com>
2023-01-13 21:27:00 -0800

    Add page info

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,15 @@ import {
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>PHS Coding Club</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="PHS Coding Club" />
+        <meta name="author" content="PHS Coding Club" />
+        <meta name="keywords" content="coding, club, phs, programming, technology, computer science" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
       <body>
         <Main />
         <NextScript />
